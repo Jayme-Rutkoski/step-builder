2095ceb338fde5e1705c2fc576a4a9093081abe0
Jayme Rutkoski <jayme-rutkoski@MacBookPro.sandhills.int>
2025-08-11 15:53:49 -0700

    Add default values to Monster data model

--- StepBuilder/StepBuilder/Application/Data/Models/Monster.swift
+++ StepBuilder/StepBuilder/Application/Data/Models/Monster.swift
@@ -7,9 +7,9 @@
 import Foundation
 
 class Monster: Codable {
-    var id: Int
-    var name: String
-    var rarity: Int
+    var id: Int = 0
+    var name: String = ""
+    var rarity: Int = 0
     
     init() { }
 }
